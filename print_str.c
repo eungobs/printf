eb0cb22b3f48cb4dca849c53bef23895c18d30a9
NewJourney21 <portiajayden21@gmail.com>
2023-05-29 17:52:49 -0700

    str

---
+++ print_str.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdarg.h>
+
+Int main(void)
+{
+	Int num = 20
+		Printf(the value of num using % "%d" is d\n" num);
+	Printf(the value of num using  %%I\n", num);
+	Return (0);
+}
