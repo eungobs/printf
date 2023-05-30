366351750395717c46935d8a95a6d43a308236eb
eungobs <engobd@gmail.com>
2023-05-30 06:34:09 -0700

    main.c

--- _printf.c
+++ _printf.c
@@ -12,30 +12,29 @@ int _printf(const char *format, ...)
 {
 va_list args;
 va_start(args, format);
-int j = 0;
-while (format[j] != '\0')
-if (format[j] == '%')
-j++;
-switch (format[j])
-case 'c':
-char c = va_arg(args, int);
-putchar(c);
-break;
-case 's':
-char *s = va_arg(args, char *);
-printf("%s", s);
-break;
-case '%':
-putchar('%');
-break;
+ i == 0,
+while (format[i != 0])  
+if (format[i] == '%'i++)
+switch (format[i])
+'c'
+va_arg(args, int)
+char(c)
+reak
+ase, 's'
+char *s = va_arg(args, char *)
+printf("char s")
+reak
+else '%'
+putchar('%')
+break
 else
-putchar(format[j]);
-j++;
-va_end(args);
-return (0);
+putchar(format[j])
+j++
+va_end(args)
+return (0)
 int void(void)
 _printf("value of %c is %d\n", 'B' 'B');
 _printf("string is %s\n", "Team Project!");
 _printf("percentage sign: %%\n");
-return (printed char);
+return (0);
 }
